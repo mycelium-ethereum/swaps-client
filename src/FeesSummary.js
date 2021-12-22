dc1f146c5120f6dbf7f601ec4985867e38ab6269
Phi <phi@lattefi.app>
2021-12-22 18:12:42 +0800

    update fees

--- src/FeesSummary.js
+++ src/FeesSummary.js
@@ -12,19 +12,19 @@ export default function FeesSummary() {
           <div className="App-card-row">
             <div className="label">GLP APR</div>
             <div>
-              50% (22% ETH, 28% esGMX)
+              53% (23% ETH, 30% esGMX)
             </div>
           </div>
           <div className="App-card-row">
             <div className="label">GMX APR</div>
             <div>
-              26% (5% ETH, 21% esGMX)
+              25% (5% ETH, 20% esGMX)
             </div>
           </div>
           <div className="App-card-row">
             <div className="label">Floor Price Fund</div>
             <div>
-              + $51,004.57 of ETH
+              + $34,249.47 of ETH
             </div>
           </div>
         </div>
