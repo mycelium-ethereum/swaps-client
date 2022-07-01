8f81600e82f20fa412520f9a1231b56ae34c8960
Dospore <tetthera@gmail.com>
2022-07-01 11:06:26 +1000

    Update header colouring

--- src/Footer.js
+++ src/Footer.js
@@ -1,15 +1,7 @@
 import React from "react";
-
-import "./Footer.css";
 import { Footer as TracerFooter } from '@tracer-protocol/tracer-ui';
 
-// import logoImg from "./img/ic_gmx_footer.svg";
-// import twitterIcon from "./img/ic_twitter.svg";
-// import discordIcon from "./img/ic_discord.svg";
-// import telegramIcon from "./img/ic_telegram.svg";
-// import githubIcon from "./img/ic_github.svg";
-// import mediumIcon from "./img/ic_medium.svg";
-// import { NavLink } from "react-router-dom";
+import "./Footer.css";
 
 export default function Footer() {
   return (
