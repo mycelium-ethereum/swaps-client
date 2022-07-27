f46645d108fe473de86d5f0c2d22583bba348270
Dospore <tetthera@gmail.com>
2022-07-27 16:28:51 +1000

    Update landing page (#21)

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,6 +1,8 @@
 import React, { useCallback } from "react";
 import Footer from "../../Footer";
-import { Link, NavLink } from "react-router-dom";
+import { 
+  NavLink 
+} from "react-router-dom";
 
 import "./Home.css";
 
@@ -8,9 +10,6 @@ import simpleSwapIcon from "../../img/ic_simpleswaps.svg";
 import costIcon from "../../img/ic_cost.svg";
 import liquidityIcon from "../../img/ic_liquidity.svg";
 import totaluserIcon from "../../img/ic_totaluser.svg";
-
-import arbitrumIcon from "../../img/ic_arbitrum_96.svg";
-import avaIcon from "../../img/ic_avalanche_96.svg";
 
 import statsIcon from "../../img/ic_stats.svg";
 import tradingIcon from "../../img/ic_trading.svg";
@@ -25,7 +24,6 @@ import {
   USD_DECIMALS,
   useChainId,
   ARBITRUM,
-  AVALANCHE,
   switchNetwork,
   getTotalVolumeSum,
 } from "../../Helpers";
@@ -34,35 +32,7 @@ import { useWeb3React } from "@web3-react/core";
 
 import { useUserStat } from "../../Api";
 
-import TokenCard from "../../components/TokenCard/TokenCard";
-
 export default function Home() {
-  // const [openedFAQIndex, setOpenedFAQIndex] = useState(null)
-  // const faqContent = [{
-  //   id: 1,
-  //   question: "What is GMX?",
-  //   answer: "GMX is a decentralized spot and perpetual exchange that supports low swap fees and zero price impact trades.<br><br>Trading is supported by a unique multi-asset pool that earns liquidity providers fees from market making, swap fees, leverage trading (spreads, funding fees & liquidations), and asset rebalancing.<br><br>Dynamic pricing is supported by Chainlink Oracles along with TWAP pricing from leading volume DEXs."
-  // }, {
-  //   id: 2,
-  //   question: "What is the GMX Governance Token? ",
-  //   answer: "The GMX token is the governance token of the GMX ecosystem, it provides the token owner voting rights on the direction of the GMX platform.<br><br>Additionally, when GMX is staked you will earn 30% of the platform-generated fees, you will also earn Escrowed GMX tokens and Multiplier Points."
-  // }, {
-  //   id: 3,
-  //   question: "What is the GLP Token? ",
-  //   answer: "The GLP token represents the liquidity users provide to the GMX platform for Swaps and Margin Trading.<br><br>To provide liquidity to GLP you <a href='https://gmx.io/buy_glp' target='_blank'>trade</a> your crypto asset BTC, ETH, LINK, UNI, USDC, USDT, MIM, or FRAX to the liquidity pool, in exchange, you gain exposure to a diversified index of tokens while earning 50% of the platform trading fees and esGMX."
-  // }, {
-  //   id: 4,
-  //   question: "What can I trade on GMX? ",
-  //   answer: "On GMX you can swap or margin trade any of the following assets: ETH, BTC, LINK, UNI, USDC, USDT, MIM, FRAX, with others to be added. "
-  // }]
-
-  // const toggleFAQContent = function(index) {
-  //   if (openedFAQIndex === index) {
-  //     setOpenedFAQIndex(null)
-  //   } else {
-  //     setOpenedFAQIndex(index)
-  //   }
-  // }
 
   const { chainId } = useChainId();
   const { active } = useWeb3React();
@@ -79,27 +49,13 @@ export default function Home() {
     fetcher: (...args) => fetch(...args).then((res) => res.json()),
   });
 
-  // AVALANCHE
-
-  const avalanchePositionStatsUrl = getServerUrl(AVALANCHE, "/position_stats");
-  const { data: avalanchePositionStats } = useSWR([avalanchePositionStatsUrl], {
-    fetcher: (...args) => fetch(...args).then((res) => res.json()),
-  });
-
-  const avalancheTotalVolumeUrl = getServerUrl(AVALANCHE, "/total_volume");
-  const { data: avalancheTotalVolume } = useSWR([avalancheTotalVolumeUrl], {
-    fetcher: (...args) => fetch(...args).then((res) => res.json()),
-  });
-
   // Total Volume
 
   const arbitrumTotalVolumeSum = getTotalVolumeSum(arbitrumTotalVolume);
-  const avalancheTotalVolumeSum = getTotalVolumeSum(avalancheTotalVolume);
 
   let totalVolumeSum = bigNumberify(0);
-  if (arbitrumTotalVolumeSum && avalancheTotalVolumeSum) {
+  if (arbitrumTotalVolumeSum) {
     totalVolumeSum = totalVolumeSum.add(arbitrumTotalVolumeSum);
-    totalVolumeSum = totalVolumeSum.add(avalancheTotalVolumeSum);
   }
 
   // Open Interest
@@ -114,26 +70,12 @@ export default function Home() {
     openInterest = openInterest.add(arbitrumPositionStats.totalShortPositionSizes);
   }
 
-  if (
-    avalanchePositionStats &&
-    avalanchePositionStats.totalLongPositionSizes &&
-    avalanchePositionStats.totalShortPositionSizes
-  ) {
-    openInterest = openInterest.add(avalanchePositionStats.totalLongPositionSizes);
-    openInterest = openInterest.add(avalanchePositionStats.totalShortPositionSizes);
-  }
-
   // user stat
   const arbitrumUserStats = useUserStat(ARBITRUM);
-  const avalancheUserStats = useUserStat(AVALANCHE);
   let totalUsers = 0;
 
   if (arbitrumUserStats && arbitrumUserStats.uniqueCount) {
     totalUsers += arbitrumUserStats.uniqueCount;
-  }
-
-  if (avalancheUserStats && avalancheUserStats.uniqueCount) {
-    totalUsers += avalancheUserStats.uniqueCount;
   }
 
   const changeNetwork = useCallback(
@@ -229,91 +171,6 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="Home-cta-section">
-        <div className="Home-cta-container default-container">
-          <div className="Home-cta-info">
-            <div className="Home-cta-info__title">Available on your preferred network</div>
-            <div className="Home-cta-info__description">GMX is currently live on Arbitrum and Avalanche.</div>
-          </div>
-          <div className="Home-cta-options">
-            <div className="Home-cta-option Home-cta-option-arbitrum">
-              <div className="Home-cta-option-icon">
-                <img src={arbitrumIcon} alt="arbitrum" />
-              </div>
-              <div className="Home-cta-option-info">
-                <div className="Home-cta-option-title">Arbitrum</div>
-                <div className="Home-cta-option-action">
-                  <Link to="/trade" className="default-btn" onClick={() => changeNetwork(ARBITRUM)}>
-                    Launch Exchange
-                  </Link>
-                </div>
-              </div>
-            </div>
-            <div className="Home-cta-option Home-cta-option-ava">
-              <div className="Home-cta-option-icon">
-                <img src={avaIcon} alt="ava" />
-              </div>
-              <div className="Home-cta-option-info">
-                <div className="Home-cta-option-title">Avalanche</div>
-                <div className="Home-cta-option-action">
-                  <Link to="/trade" className="default-btn" onClick={() => changeNetwork(AVALANCHE)}>
-                    Launch Exchange
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="Home-token-card-section">
-        <div className="Home-token-card-container default-container">
-          <div className="Home-token-card-info">
-            <div className="Home-token-card-info__title">Two tokens create our ecosystem</div>
-          </div>
-          <TokenCard />
-        </div>
-      </div>
-
-      {/* <div className="Home-video-section">
-        <div className="Home-video-container default-container">
-          <div className="Home-video-block">
-            <img src={gmxBigIcon} alt="gmxbig" />
-          </div>
-        </div>
-      </div> */}
-      {/* <div className="Home-faqs-section">
-        <div className="Home-faqs-container default-container">
-          <div className="Home-faqs-introduction">
-            <div className="Home-faqs-introduction__title">FAQs</div>
-            <div className="Home-faqs-introduction__description">Most asked questions. If you wish to learn more, please head to our Documentation page.</div>
-            <a href="https://gmxio.gitbook.io/gmx/" className="default-btn Home-faqs-documentation">Documentation</a>
-          </div>
-          <div className="Home-faqs-content-block">
-            {
-              faqContent.map((content, index) => (
-                <div className="Home-faqs-content" key={index} onClick={() => toggleFAQContent(index)}>
-                  <div className="Home-faqs-content-header">
-                    <div className="Home-faqs-content-header__icon">
-                      {
-                        openedFAQIndex === index ? <FiMinus className="opened" /> : <FiPlus className="closed" />
-                      }
-                    </div>
-                    <div className="Home-faqs-content-header__text">
-                      { content.question }
-                    </div>
-                  </div>
-                  <div className={ openedFAQIndex === index ? "Home-faqs-content-main opened" : "Home-faqs-content-main" }>
-                    <div className="Home-faqs-content-main__text">
-                      <div dangerouslySetInnerHTML={{__html: content.answer}} >
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              ))
-            }
-          </div>
-        </div>
-      </div> */}
       <Footer />
     </div>
   );
