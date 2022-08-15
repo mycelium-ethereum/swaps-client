bc5d9ae8d166fce0695039328b92d601791e36da
CalabashSquash <my_name_uhh@protonmail.com>
2022-08-15 15:54:54 +1000

    Correct API for graph

--- src/Api/common.js
+++ src/Api/common.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const chainlinkClient = createClient("https://api.thegraph.com/subgraphs/name/deividask/chainlink");
-export const arbitrumGraphClient = createClient("https://thegraph.com/hosted-service/subgraph/mycelium-ethereum/myc-swaps-stats");
+export const arbitrumGraphClient = createClient("https://api.thegraph.com/subgraphs/name/mycelium-ethereum/myc-swaps-stats");
 export const arbitrumTestnetGraphClient = createClient("https://api.thegraph.com/subgraphs/name/tracer-protocol/arbitrum-rinkeby-gmx-stats");
 export const avalancheGraphClient = createClient("https://api.thegraph.com/subgraphs/name/gmx-io/gmx-avalanche-stats");
 export const nissohGraphClient = createClient("https://api.thegraph.com/subgraphs/name/dospore/gmx-vault");
