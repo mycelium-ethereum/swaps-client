e4b362a20cdda176fe01074c71f2af2eebc07143
iflp <5000825+iflp@users.noreply.github.com>
2022-08-16 19:35:27 +1000

    stub ui call

--- src/Api/index.js
+++ src/Api/index.js
@@ -36,7 +36,7 @@ import {
   getInfoTokens,
   isAddressZero,
   helperToast,
-  getSupplyUrl
+  getSupplyUrl,
 } from "../Helpers";
 import { getTokens, getTokenBySymbol, getWhitelistedTokens } from "../data/Tokens";
 
@@ -463,18 +463,7 @@ export function useStakedMycSupply(library, active) {
 }
 
 export function useHasOutdatedUi() {
-  const url = getServerUrl(ARBITRUM, "/ui_version");
-  const { data, mutate } = useSWR([url], {
-    fetcher: (...args) => fetch(...args).then((res) => res.text()),
-  });
-
-  let hasOutdatedUi = false;
-
-  if (data && parseFloat(data) > parseFloat(UI_VERSION)) {
-    hasOutdatedUi = true;
-  }
-
-  return { data: hasOutdatedUi, mutate };
+  return { data: false };
 }
 
 export function useTCRPrice(chainId, libraries, active) {
@@ -498,7 +487,7 @@ export function useTCRPrice(chainId, libraries, active) {
 }
 
 export function useTotalTCRSupply() {
-  const tcrSupplyUrl = getSupplyUrl()
+  const tcrSupplyUrl = getSupplyUrl();
 
   const { data: tcrSupply, mutate: updateTCRSupply } = useSWR([tcrSupplyUrl], {
     fetcher: (...args) => fetch(...args, { headers: { "Content-Type": "application/json" } }).then((res) => res.json()),
