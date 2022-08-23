a8066096648bd998adb8a67de247d7c3579f6e0a
Dospore <tetthera@gmail.com>
2022-08-23 15:55:25 +1000

    Add MM apr

--- src/Helpers.js
+++ src/Helpers.js
@@ -147,6 +147,9 @@ export const TRIGGER_PREFIX_BELOW = "<";
 
 export const MIN_PROFIT_BIPS = 0;
 
+// USD tokens per interval given by kurtis
+export const MM_TOKENS_PER_INTERVAL = ethers.BigNumber.from('0');
+
 export const MLP_POOL_COLORS = {
   ETH: "#6062a6",
   BTC: "#F7931A",
@@ -2789,7 +2792,9 @@ export function getProcessedData(
     data.mlpSupplyUsd && data.mlpSupplyUsd.gt(0)
       ? data.feeMlpTrackerAnnualRewardsUsd.mul(BASIS_POINTS_DIVISOR).div(data.mlpSupplyUsd)
       : bigNumberify(0);
-  data.mlpAprTotal = data.mlpAprForNativeToken.add(data.mlpAprForEsMyc);
+
+  data.mmApr = MM_TOKENS_PER_INTERVAL.mul(SECONDS_PER_YEAR).mul(BASIS_POINTS_DIVISOR).div(data.mlpSupplyUsd);
+  data.mlpAprTotal = data.mlpAprForNativeToken.add(data.mlpAprForEsMyc).add(data.mmApr);
 
   data.totalMlpRewardsUsd = data.stakedMlpTrackerRewardsUsd.add(data.feeMlpTrackerRewardsUsd);
 
