5b5a01709c4a80900bde563116e20adac231b4f0
Dospore <tetthera@gmail.com>
2022-08-24 16:26:32 +1000

    Rename label

--- src/views/Stake/StakeV2.js
+++ src/views/Stake/StakeV2.js
@@ -879,18 +879,7 @@ export default function StakeV2({ setPendingTxns, connectWallet, trackAction }) 
                   </div>
                 </StakeV2Styled.RewardsBannerRow>
                 <StakeV2Styled.RewardsBannerRow>
-                  <StakeV2Styled.RewardsBannerText secondary>Market Making APR</StakeV2Styled.RewardsBannerText>
-                  <StakeV2Styled.RewardsBannerText large inline>
-                    <Tooltip
-                      handle={`${formatKeyAmount(processedData, "mmApr", 2, 2, true)}%`}
-                      position="right-bottom"
-                      renderContent={() => "Market Making APR is sourced from the spread of the traded markets and is realised by MLP holders through the appreciation of the MLP token."
-                      }
-                    />
-                  </StakeV2Styled.RewardsBannerText>
-                </StakeV2Styled.RewardsBannerRow>
-                <StakeV2Styled.RewardsBannerRow>
-                  <StakeV2Styled.RewardsBannerText secondary>APR</StakeV2Styled.RewardsBannerText>
+                  <StakeV2Styled.RewardsBannerText secondary>Total APR</StakeV2Styled.RewardsBannerText>
                   <StakeV2Styled.RewardsBannerText large inline>
                     <Tooltip
                       handle={`${formatKeyAmount(processedData, "mlpAprTotal", 2, 2, true)}%`}
