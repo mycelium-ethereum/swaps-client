53d1bfdff429c24c4e6c304aaf8776b918936a37
Tim Jones <t27.jones@connect.qut.edu.au>
2022-11-03 09:57:28 +1000

    Add countdown to trading comp banner

--- src/components/Exchange/TradingCompBanner.js
+++ src/components/Exchange/TradingCompBanner.js
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import cx from "classnames";
+import Countdown from "react-countdown";
 import * as Styles from "./TradingCompBanner.styles";
+
+const COMP_END_TS = 1667952000000; // 2022-11-09 10:00:00 AEST
 
 export default function TradingCompBanner() {
   const [showBanner, setShowBanner] = useState(true);
@@ -10,6 +13,24 @@ export default function TradingCompBanner() {
     setShowBanner(false);
     window.localStorage.setItem("hasDismissedReferralsBanner", "true");
     document.querySelector(".Exchange")?.classList.remove("ReferralsBannerActive");
+  };
+
+  const countdownRenderer = ({ days, hours, minutes, seconds, completed }) => {
+    if (completed) {
+      return (
+        <span>
+          0d {hours}h {minutes}m {seconds}s
+        </span>
+      );
+    } else {
+      return (
+        <span>
+          {`${days ? `${days}d` : ``} ${hours ? `${hours}h` : ``} ${minutes ? `${minutes}m` : ``} ${
+            seconds ? `${seconds}s` : ``
+          }`}
+        </span>
+      );
+    }
   };
 
   useEffect(() => {
@@ -31,9 +52,11 @@ export default function TradingCompBanner() {
           <Styles.LiveIcon /> <Styles.GreenText>Live</Styles.GreenText>
         </Styles.FlexContainer>
         <Styles.BannerTitle>Referrals Competition</Styles.BannerTitle>
-        <span>Refer more = Earn More</span>
+        <span>
+          Time left: <Countdown date={COMP_END_TS} renderer={countdownRenderer} intervalDelay={0} precision={3} />
+        </span>
         <Link to="/referrals#commissions">
-          <Styles.GenerateButton>Generate & Share your code today to play.</Styles.GenerateButton>
+          <Styles.GenerateButton>Generate & share your code today to play.</Styles.GenerateButton>
         </Link>
       </Styles.TextContainer>
       <Styles.CloseButton onClick={onClose} />
