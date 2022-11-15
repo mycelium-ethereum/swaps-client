1a7106020fa749af907218eda7f79c130fd3b649
Dospore <tetthera@gmail.com>
2022-11-15 17:48:42 +1100

    Hide on leaderboard

--- src/views/Referrals/Referrals.js
+++ src/views/Referrals/Referrals.js
@@ -516,7 +516,7 @@ export default function Referral(props) {
               setIsClaimModalOpen={setIsClaimModalOpen}
             />
           )}
-          {active && (
+          {active && currentView !== LEADERBOARD && (
             <ReferralsClaim
               isVisible={isClaimModalOpen}
               setIsVisible={setIsClaimModalOpen}
