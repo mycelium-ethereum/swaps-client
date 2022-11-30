5a16a4f80f669ab8ce35dccbfb415fab30a99b0a
Dospore <tetthera@gmail.com>
2022-11-30 13:28:02 +1100

    Breakdown func

--- src/data/Fees.js
+++ src/data/Fees.js
@@ -40,7 +40,9 @@ const FEES = {
 
 const TWO_WEEKS = SECONDS_PER_WEEK * 2;
 const FORTNIGHT_START = 1662508800;
-export const currentFortnight = FORTNIGHT_START + TWO_WEEKS * Math.floor(((Math.floor(Date.now() / 1000) - FORTNIGHT_START) / TWO_WEEKS))
+const SECONDS_SINCE_START = Math.floor(Date.now() / 1000) - FORTNIGHT_START;
+const FORTNIGHTS_SINCE_START = Math.floor(SECONDS_SINCE_START / TWO_WEEKS)
+export const currentFortnight = FORTNIGHT_START + (TWO_WEEKS * FORTNIGHTS_SINCE_START)
 
 export function getFeeHistory(chainId) {
   return FEES[chainId].concat([]).reverse();
